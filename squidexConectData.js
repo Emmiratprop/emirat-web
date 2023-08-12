9d5354c4c3aec523ec7445c2c9701de31907486f
Flori O <floroldani@hotmail.com>
2023-08-11 23:23:31 -0300

    automatizar token

--- squidexConectData.js
+++ squidexConectData.js
@@ -1,58 +1,115 @@
 
-const accessToken = process.env.NEXT_PUBLIC_SQUIDEX_TOKEN
+// Esta función obtiene el token de la caché
+function getTokenFromCache() {
+  // Lógica para obtener el token de la caché, por ejemplo, localStorage o sessionStorage
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('access_token');
+  }
+  return null;
+}
 
 
-// function makeRequest(url, body) {
-//   const token = getTokenFromCache();
-  
-//   if (!token) {
-//       token = getToken(clientId, clientSecret);
-      
-//       storeTokenInCache(token, days: 30);
-//   }
-  
-//   const response = makeRequestToSquidex(url, body);
-  
-//   // Token has probably expired. 
-//   if (response.status == 401) {
-//       // Request the token again.
-//       token = getToken(clientId, clientSecret);
-      
-//       storeTokenInCache(token, days: 30);
-      
-//       // Try the request again.
-//       response = makeRequestToSquidex(url, body);
-//   }
-  
-//   // You can still have a 401 here, but this very likely not an epxired token then.
-//   return response;
-// }
+
+// Esta función guarda el token en la caché
+function storeTokenInCache(token) {
+  // Lógica para guardar el token en la caché, por ejemplo, localStorage o sessionStorage
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('access_token', token);
+  }
+}
 
 
+
+
+// Esta función obtiene el token del servidor usando las credenciales de cliente
+async function getToken(clientId, clientSecret) {
+  const response = await fetch('https://cloud.squidex.io/identity-server/connect/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      scope: 'squidex-api',
+      client_secret: clientSecret,
+      client_id: clientId,
+      grant_type: 'client_credentials'
+    })
+  });
+
+  const data = await response.json();
+  const token = data.access_token;
+  return token;
+}
+
+
+
+
+
+// Esta función realiza la solicitud a Squidex API con el token proporcionado
 export default async function fetchAPI(query, { variables } = {}) {
+  const clientId = process.env.NEXT_PUBLIC_SQUIDEX_CLIENT_ID;
+  const clientSecret = process.env.NEXT_PUBLIC_SQUIDEX_CLIENT_SECRET;
+  
+  let accessToken = getTokenFromCache();
+  
+  if (!accessToken) {
+    accessToken = await getToken(clientId, clientSecret);
+    storeTokenInCache(accessToken);
+  }
+  
   try {
     const res = await fetch(process.env.NEXT_PUBLIC_SQUIDEX_API_URL, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        'Authorization':  `Bearer ${accessToken}`
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
       },
       body: JSON.stringify({
         query,
-        variables,
+        variables
       }),
     });
+    
     const json = await res.json();
-    console.log('json',json)
   
     if (json.errors) {
       console.error(json.errors);
-      throw new Error("Failed to fetch API");
+      throw new Error('Failed to fetch API');
     }
   
     return json.data;
-   }catch(error){
-    console.log('faild catch fetch api: ', error)
+  } catch (error) {
+    console.log('Failed to fetch API: ', error);
     throw error; 
-   }
   }
+}
+
+
+
+// export default async function fetchAPI(query, { variables } = {}) {
+//   try {
+//     const res = await fetch(process.env.NEXT_PUBLIC_SQUIDEX_API_URL, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         'Authorization':  `Bearer ${accessToken}`
+//       },
+//       body: JSON.stringify({
+//         query,
+//         variables,
+//       }),
+//     });
+//     const json = await res.json();
+//     console.log('json',json)
+  
+//     if (json.errors) {
+//       console.error(json.errors);
+//       throw new Error("Failed to fetch API");
+//     }
+  
+//     return json.data;
+//    }catch(error){
+//     console.log('faild catch fetch api: ', error)
+//     throw error; 
+//    }
+//   }
